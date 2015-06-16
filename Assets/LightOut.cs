c8755bcc5f1b8c35604cb77f56348abf8bce7472
Rigved Deshpande <rigved.deshpande@gmail.com>
2015-06-16 15:53:18 -0700

    Basic version with start screen and start game button

---
+++ Assets/LightOut.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightOut : MonoBehaviour {
+
+	public float Battery;
+	private Light Torch;
+
+	void Start () {
+		Battery = 100f;
+		gameObject.SetActive (true);
+		Torch = GetComponent<Light> ();
+	}
+
+	void Update () {
+
+		if (Battery > 0f) {
+			Battery -= .05f;
+		}
+
+		else if(Battery <= 0) {
+			gameObject.SetActive (false);
+		}
+
+		Torch.intensity = 4f * Battery / 100;
+
+	}
+}
+
+
+
---
+++ Assets/MenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+	
+	public Button PlayText;
+	public Button ExitText;
+	
+	void start() 
+	{
+		PlayText = PlayText.GetComponent<Button> ();
+		ExitText = ExitText.GetComponent<Button> ();
+	}
+
+	public void PlayPress()
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void ExitPress()
+	{
+		Application.Quit ();
+	}
+}
